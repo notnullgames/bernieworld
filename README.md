9a810022143457eac4d3d9ae84d790a3ab291f9b
David Konsumer <konsumer@jetboystudio.com>
2021-10-13 23:55:26 -0700

    click to play (for sound) and font

--- README.md
+++ README.md
@@ -6,4 +6,6 @@ You can see the [current version here](https://notnullgames.github.io/bernieworl
 
 I love [Super Bernie World](https://eniko.itch.io/superbernieworld), but they use a proprietary game-engine that they cant't release as open-source. For fun, I ported it to [kaboom](https://kaboomjs.com/) using their content. All game-content credit goes to [eniko](https://eniko.itch.io/). This makes it so you can edit levels and make new ones!
 
-I had to reverse-enginner the maps a bit, as the object-images weren't included, but maps are now editable in [tiled](https://www.mapeditor.org/) and loaded with [my plugin](https://github.com/notnullgames/tiled-kaboom). I went through and added differnt properties to make things easier for me to work with, btu the goal is a total colone.
+I had to reverse-enginner the maps a bit, as the object-images weren't included, but maps are now editable in [tiled](https://www.mapeditor.org/) and loaded with [my plugin](https://github.com/notnullgames/tiled-kaboom). I went through and added differnt properties to make things easier for me to work with, but the goal is to be a perfect clone, but editable.
+
+I also had to convert the font into a uniform format for kaboom.
--- game.js
+++ game.js
@@ -1,15 +1,64 @@
+/* global fetch */
 import kaboom from 'kaboom'
 import tiledKaboom from 'tiled-kaboom'
-import map from './assets/maps/1-1.json' assert { type: "json" }
 
-const k = kaboom({
-    plugins: [ tiledKaboom ]
-})
+async function loadMap (name) {
+  const map = await fetch(`./assets/maps/${name}.json`).then(r => r.json())
+  const { levels, key, mapObj } = await k.loadTiledMap(map, './assets/maps/')
 
-const { sprites, levels, key, mapObj } = await k.loadTiledMap(map, './assets/maps/')
-console.log({ sprites, levels, key, mapObj })
+  // categorize layers
+  const layersAll = {}
+  const layersObject = []
+  const layersTiled = []
+  mapObj.layers.forEach(layer => {
+    layersAll[layer.name] = layer
+    if (layer.type === 'tilelayer') {
+      layersTiled.push(layer.name)
+    } else if (layer.type === 'objectgroup') {
+      layersObject.push(layer.name)
+    }
+  })
 
-// initial load
-for (let level of levels) {
-  k.addLevel(level, { width: 16, height: 16, ...key })
+  // create a scene and add the layers
+  k.scene(name, () => {
+    k.layers(Object.keys(layersAll), layersObject[0] || layersTiled[layersTiled.length - 1])
+    for (const l in layersTiled) {
+      k.addLevel(levels[l], { width: mapObj.tileheight, height: mapObj.tilewidth, ...key })
+    }
+  })
 }
+
+let k
+async function setup () {
+  document.getElementById('welcome').remove()
+  k = kaboom({
+    plugins: [tiledKaboom]
+  })
+
+  await loadMap('1-1')
+  k.go('1-1')
+
+  // load sounds
+  await Promise.all([
+    'bounce',
+    'castle-fast',
+    'castle',
+    'chat',
+    'coin',
+    'death',
+    'fire',
+    'flag',
+    'invincibility',
+    'jump',
+    'overworld-fast',
+    'overworld',
+    'powdown',
+    'powup',
+    'reveal',
+    'shoot',
+    'stomp',
+    'victory'
+  ].map(name => k.loadSound(name, `assets/sounds/${name}.ogg`)))
+}
+
+window.addEventListener('click', setup)
